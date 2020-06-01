5dbc974b01c6f98f09eaf77a71fc1904e1c7c606
tricksterkid <deighton.zakelijk@gmail.com>
2020-06-01 19:03:44 +0200

    VERSION 8 - Switch implemented, start second interior.

---
+++ BPW 2 Official/Assets/Scripts/InteriorSwitch1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InteriorSwitch1 : MonoBehaviour
+{
+    public GameObject interior1;
+    public GameObject interior2;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            interior1.SetActive(false);
+            interior2.SetActive(true);
+            Debug.Log("Switched to interior 2");
+        }
+    }
+
+}
---
+++ BPW 2 Official/Assets/Scripts/InteriorSwitch2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InteriorSwitch2 : MonoBehaviour
+{
+    public GameObject interior1;
+    public GameObject interior2;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            interior1.SetActive(true);
+            interior2.SetActive(false);
+            Debug.Log("Switched to interior 1");
+        }
+    }
+
+}
