d09ca4e407f8c9e4791c55e700a9d37181903f79
tricksterkid <deighton.zakelijk@gmail.com>
2020-06-03 10:04:34 +0200

    VERSION 11 - Lighting.

---
+++ BPW 2 Official/Assets/Scripts/StartScreenLight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class StartScreenLight : MonoBehaviour
+{
+    public GameObject standardLight;
+    public GameObject specialLight;
+
+    void OnMouseOver()
+    {
+        standardLight.SetActive(false);
+        specialLight.SetActive(true);
+
+        Debug.Log("Changed lighting to special");
+    }
+
+    void OnMouseExit()
+    {
+        standardLight.SetActive(true);
+        specialLight.SetActive(false);
+        Debug.Log("Changed lighting to normal");
+    }
+
+}
