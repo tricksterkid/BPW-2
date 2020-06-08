5d8cca5745400376619958134c7a2bff98fa807b
tricksterkid <deighton.zakelijk@gmail.com>
2020-06-08 12:50:07 +0200

    VERSION 14 - Added post processing.

---
+++ BPW 2 Official/Assets/Scripts/GoOutside1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoOutside1 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Level02");
+        }
+    }
+}
---
+++ BPW 2 Official/Assets/Scripts/GoOutside2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoOutside2 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Level03");
+        }
+    }
+}
