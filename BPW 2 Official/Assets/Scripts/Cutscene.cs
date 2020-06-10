76f65e7efb1c48758adb286d8612691b3992bf40
tricksterkid <deighton.zakelijk@gmail.com>
2020-06-10 13:56:57 +0200

    VERSION 15 - Cutscene setup.

---
+++ BPW 2 Official/Assets/Scripts/Cutscene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    public GameObject Text1;
+    public GameObject Text2;
+    public GameObject Text3;
+
+    public GameObject Camera;
+    public GameObject Player;
+
+    private Animation cam;
+    private Animation playerMove;
+    private Animation playerJump;
+
+    void Start()
+    {
+        cam = Camera.GetComponent<Animation>();
+        playerMove = Player.GetComponent<Animation>();
+        playerJump = Player.GetComponent<Animation>();
+        StartCoroutine(CutscenePlay());
+    }
+
+    IEnumerator CutscenePlay()
+    {
+        playerMove.Play("CharacterWalk");
+        cam.Play("CameraMovement");
+        yield return new WaitForSeconds(2);
+        Text1.SetActive(true);
+        yield return new WaitForSeconds(2);
+        Text1.SetActive(false);
+        playerJump.Play("CharacterJump");
+        yield return new WaitForSeconds(2);
+        Text2.SetActive(true);
+        yield return new WaitForSeconds(2);
+        Text2.SetActive(false);
+        yield return new WaitForSeconds(2);
+        Text3.SetActive(true);
+    }
+}
