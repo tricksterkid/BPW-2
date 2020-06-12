c10ee472fdddee90d1c95381a21211c39b5ee0a3
tricksterkid <deighton.zakelijk@gmail.com>
2020-06-12 13:15:11 +0200

    VERSION 17 - Finished core gameplay and assignment requirements.

--- BPW 2 Official/Assets/Scripts/Cutscene.cs
+++ BPW 2 Official/Assets/Scripts/Cutscene.cs
@@ -10,17 +10,36 @@ public class Cutscene : MonoBehaviour
     public GameObject Camera;
     public GameObject Player;
 
+    public GameObject Cloud1;
+    public GameObject Cloud2;
+    public GameObject Cloud3;
+    public GameObject Cloud4;
+
     private Animation cam;
     private Animation playerMove;
     private Animation playerJump;
+    private Animation playerShake;
     private Animation playerDissolve;
+
+    private Animation appearCloud1;
+    private Animation appearCloud2;
+    private Animation appearCloud3;
+    private Animation appearCloud4;
 
     void Start()
     {
         cam = Camera.GetComponent<Animation>();
         playerMove = Player.GetComponent<Animation>();
         playerJump = Player.GetComponent<Animation>();
+        playerShake = Player.GetComponent<Animation>();
         playerDissolve = Player.GetComponent<Animation>();
+
+        appearCloud1 = Cloud1.GetComponent<Animation>();
+        appearCloud2 = Cloud2.GetComponent<Animation>();
+        appearCloud3 = Cloud3.GetComponent<Animation>();
+        appearCloud4 = Cloud4.GetComponent<Animation>();
+
+
         StartCoroutine(CutscenePlay());
     }
 
@@ -32,11 +51,20 @@ public class Cutscene : MonoBehaviour
         Text1.SetActive(true);
         yield return new WaitForSeconds(2);
         Text1.SetActive(false);
-        playerJump.Play("CharacterJump");
+        Cloud1.SetActive(true);
+        Cloud2.SetActive(true);
+        Cloud3.SetActive(true);
+        Cloud4.SetActive(true);
+        appearCloud1.Play("Cloud1");
+        appearCloud2.Play("Cloud2");
+        appearCloud3.Play("Cloud3");
+        appearCloud4.Play("Cloud4");
         yield return new WaitForSeconds(2);
+        playerJump.Play("CharacterJump");
         Text2.SetActive(true);
         yield return new WaitForSeconds(2);
         Text2.SetActive(false);
+        playerShake.Play("CharacterShake");
         yield return new WaitForSeconds(2);
         playerDissolve.Play("CharacterDissolve");
         yield return new WaitForSeconds(2);
