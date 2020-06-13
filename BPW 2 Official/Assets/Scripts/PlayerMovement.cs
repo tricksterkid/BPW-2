8c28de3ee0e1d352ea4004983aa316bf78710249
tricksterkid <deighton.zakelijk@gmail.com>
2020-06-13 12:49:19 +0200

    VERSION 18 - Character animations.

--- BPW 2 Official/Assets/Scripts/PlayerMovement.cs
+++ BPW 2 Official/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rigidBody;
+    public Animator animator;
     Vector2 movement;
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
